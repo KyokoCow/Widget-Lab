80a1ce6960018cf203b5dad5568f8b15c7ced81e
kyoko <kyoko@example.com>
2026-05-03 21:31:01 +0900

    プロトタイプ

--- lib/main.dart
+++ lib/main.dart
@@ -7,115 +7,294 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+/* =========================
+   データモデル
+========================= */
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class Sample {
+  final String id;
   final String title;
+  final String code;
+  final Widget Function() builder;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  const Sample({
+    required this.id,
+    required this.title,
+    required this.code,
+    required this.builder,
+  });
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+/* =========================
+   サンプル一覧（10個）
+========================= */
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+final List<Sample> samples = [
+  Sample(
+    id: "text",
+    title: "Text",
+    code: 'Text("Hello Flutter")',
+    builder: () => const Center(child: Text("Hello Flutter")),
+  ),
+
+  Sample(
+    id: "container",
+    title: "Container",
+    code: '''
+Container(
+  width: 100,
+  height: 100,
+  color: Colors.blue,
+)
+''',
+    builder: () => Center(
+      child: Container(width: 100, height: 100, color: Colors.blue),
+    ),
+  ),
+
+  Sample(
+    id: "row",
+    title: "Row",
+    code: '''
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [Icon(Icons.star), Icon(Icons.star)],
+)
+''',
+    builder: () => const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Icon(Icons.star), Icon(Icons.star)],
+    ),
+  ),
+
+  Sample(
+    id: "column",
+    title: "Column",
+    code: '''
+Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [Text("A"), Text("B")],
+)
+''',
+    builder: () => const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Text("A"), Text("B")],
+    ),
+  ),
+
+  Sample(
+    id: "button",
+    title: "ElevatedButton",
+    code: '''
+ElevatedButton(
+  onPressed: () {},
+  child: Text("押す"),
+)
+''',
+    builder: () => Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text("押す"),
+      ),
+    ),
+  ),
+
+  Sample(
+    id: "textfield",
+    title: "TextField",
+    code: 'TextField()',
+    builder: () => const Padding(
+      padding: EdgeInsets.all(16),
+      child: TextField(),
+    ),
+  ),
+
+  Sample(
+    id: "list",
+    title: "ListView",
+    code: '''
+ListView.builder(
+  itemCount: 10,
+  itemBuilder: (_, i) => ListTile(title: Text("Item \$i")),
+)
+''',
+    builder: () => ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, i) => ListTile(title: Text("Item $i")),
+    ),
+  ),
+
+  Sample(
+    id: "card",
+    title: "Card",
+    code: '''
+Card(
+  child: Padding(
+    padding: EdgeInsets.all(16),
+    child: Text("Card"),
+  ),
+)
+''',
+    builder: () => const Center(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Text("Card"),
+        ),
+      ),
+    ),
+  ),
+
+  Sample(
+    id: "dialog",
+    title: "Dialog",
+    code: '''
+showDialog(
+  context: context,
+  builder: (_) => AlertDialog(title: Text("Hello")),
+)
+''',
+    builder: () => Builder(
+      builder: (context) => Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (_) =>
+              const AlertDialog(title: Text("Hello Dialog")),
+            );
+          },
+          child: const Text("ダイアログ表示"),
+        ),
+      ),
+    ),
+  ),
+
+  Sample(
+    id: "animated",
+    title: "AnimatedContainer",
+    code: '''
+AnimatedContainer(
+  duration: Duration(seconds: 1),
+  width: 100→200,
+)
+''',
+    builder: () => const AnimatedSample(),
+  ),
+];
+
+/* =========================
+   メイン画面
+========================= */
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Sample selected = samples.first;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: const Text("Flutter Sampler")),
+      body: Row(
+        children: [
+          // 左：リスト
+          Container(
+            width: 220,
+            color: Colors.grey[200],
+            child: ListView(
+              children: samples.map((s) {
+                return ListTile(
+                  title: Text(s.title),
+                  selected: selected.id == s.id,
+                  onTap: () {
+                    setState(() {
+                      selected = s;
+                    });
+                  },
+                );
+              }).toList(),
             ),
-          ],
-        ),
+          ),
+
+          // 右：プレビュー＋コード
+          Expanded(
+            child: Column(
+              children: [
+                // プレビュー
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                    child: Center(child: selected.builder()),
+                  ),
+                ),
+
+                // コード
+                Expanded(
+                  child: Container(
+                    color: Colors.black,
+                    padding: const EdgeInsets.all(12),
+                    child: SingleChildScrollView(
+                      child: SelectableText(
+                        selected.code,
+                        style: const TextStyle(
+                          fontFamily: 'monospace',
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+/* =========================
+   Animatedサンプル
+========================= */
+
+class AnimatedSample extends StatefulWidget {
+  const AnimatedSample({super.key});
+
+  @override
+  State<AnimatedSample> createState() => _AnimatedSampleState();
+}
+
+class _AnimatedSampleState extends State<AnimatedSample> {
+  bool big = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          big = !big;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(seconds: 1),
+        width: big ? 200 : 100,
+        height: big ? 200 : 100,
+        color: Colors.red,
       ),
     );
   }
